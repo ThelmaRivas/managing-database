adfaa61b56be0b89ed63e3e9c719e84a46b24b36
ThelmaRivas <thelma.rivas00@gmail.com>
2023-11-30 16:23:33 -0600

    Main menu function
    Create function with a switch statement to display the user all the available functions

--- Develop/server.js
+++ Develop/server.js
@@ -18,3 +18,85 @@ connection.connect((err) => {
     console.log('Connected to the database!');
     mainMenu();
 });
+
+
+// Main menu functions and inquirer prompts
+
+// Function for main menu
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Update an employee manager',
+                    'View employees by manager',
+                    'View employees by department',
+                    'Delete a department',
+                    'Delete a role',
+                    'Delete an employee',
+                    'View total utilized budget by department',
+                    'Exit',
+                ],
+            },
+        ])
+        .then((answers) => {
+            switch (answers.choice) {
+                case 'View all departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all roles':
+                    viewAllRoles();
+                    break;
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'Update an employee manager':
+                    updateEmployeeManager();
+                    break;
+                case 'View employees by manager':
+                    viewEmployeesByManager();
+                    break;
+                case 'View employees by department':
+                    viewEmployeesByDepartment();
+                    break;
+                case 'Delete a department':
+                    deleteDepartment();
+                    break;
+                case 'Delete a role':
+                    deleteRole();
+                    break;
+                case 'Delete an employee':
+                    deleteEmployee();
+                    break;
+                case 'View total utilized budget by department':
+                    calculateDepartmentBudget();
+                    break;
+                case 'Exit':
+                    console.log('Exiting...');
+                    connection.end();
+                    break;
+            }
+        });
+}
